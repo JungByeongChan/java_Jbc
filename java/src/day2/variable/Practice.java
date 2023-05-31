8cbe0ac712e8bcb1275d54ca3aba55be5ec01e51
JungByeongChan <gun6377@naver.com>
2023-05-31 12:41:30 +0900

    자바 변수 연습 예제

---
+++ java/src/day2/variable/Practice.java
@@ -0,0 +1,32 @@
+package day2.variable;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		char ch1 = 'A';
+		System.out.println(ch1);
+		/* 1. 아래 코드에서 잘못된 부분을 찾아 수정하세요.
+		 * char ch1 = "A";
+		 * 문자는 ""가 아닌 ''로 감싸여야한다. 
+		 **/
+		
+		
+		/* 2. 국어, 영어 ,수학 성적을 입력하여 평균을 구하려 한다. 이 떄 필요한 변수들을 선언하세요.
+		 * */
+		
+		int kor, eng, math;
+		double avg;
+		
+		/*int korean = 100;
+		int english = 82;
+		int math = 33;
+		
+		double B = (korean + english + math) / 3;
+				System.out.println(B);
+				*/
+		
+		
+			
+	}
+
+}
