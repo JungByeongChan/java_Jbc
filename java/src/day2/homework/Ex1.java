381066f12d25a58ce2d281e4bcd32e4fc82db5ff
JungByeongChan <gun6377@naver.com>
2023-06-01 09:50:14 +0900

    5.30 과제풀이

--- java/src/day2/homework/Ex1.java
+++ java/src/day2/homework/Ex1.java
@@ -1,10 +1,35 @@
 package day2.homework;
 
+import java.util.Scanner;
+
 public class Ex1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		Scanner sc = new Scanner(System.in);
+		
 
+		System.out.println("두 정수와 산술 연살자를 입력하시오");
+
+		String str = sc.nextLine();
+
+
+
+		System.out.println(str);
+		*/
+		int num1, num2;
+		char opertator;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("두 정수와 산술연산자를 입력하세요 (예:1 + 2)");
+		num1 = sc.nextInt();
+		opertator = sc.next().charAt(0);
+		num2 = sc.nextInt();
+		System.out.println("" + num1 + opertator + num2);
+		
+		sc.close();
+		
 	}
 
 }
--- java/src/day2/homework/Ex2.java
+++ java/src/day2/homework/Ex2.java
@@ -1,10 +1,55 @@
 package day2.homework;
 
+import java.util.*;
+
 public class Ex2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		Scanner sc = new Scanner(System.in);
 
+
+
+		System.out.println("정수를 입력하시오");
+
+		int num1 = sc.nextInt();
+
+		System.out.println("정수를 입력하시오");
+
+		int num2 = sc.nextInt();
+
+		
+
+		System.out.println(num1 + num2);
+
+		System.out.println(num1 - num2);
+
+		System.out.println(num1 * num2);
+
+		System.out.println((double)num1 / num2);
+
+		System.out.println(num1 % num2);
+		 */
+		System.out.println("두 정수를 입력하시오");// 스캐너 생성
+		Scanner sc = new Scanner(System.in);// 정수 입력 
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		// 두 정수의 산술연산 계산
+		int sum = num1 + num2;
+		int sub = num1 - num2;
+		int mul = num1 * num2;
+		double div = num1 / (double)num2;
+		int mod = num1 % num2;
+		
+		System.out.println(num1 + "+" + num2 + "=" + sum);
+		System.out.println(num1 + "-" + num2 + "=" + sub);
+		System.out.println(num1 + "*" + num2 + "=" + mul);
+		System.out.println(num1 + "/" + num2 + "=" + div);
+		System.out.println(num1 + "%" + num2 + "=" + mod);
+		//System.out.println(num1 + "%" + num2 + "=" + (num1 % num2); 도 가능
+		// (* / %)는 (+ -) 보다 우선순위가 높기때문에 ()가 필요없지만, (+ -)는 우선순위가 낮기때문에 ()가 필요하다.
+				
+		sc.close();
 	}
 
 }
