97a2ad185b68c5097d8cab400fd4b22278d9d1a0
JungByeongChan <gun6377@naver.com>
2023-06-01 23:15:26 +0900

    자바 조건식 과제

---
+++ java/src/day3/homework/Ex5.java
@@ -0,0 +1,48 @@
+package src.day3.homework;
+
+import java.utll.Scanner
+
+import src.day3.operator.String;
+
+public class Ex5 {
+
+	public static void main(String[] args) {
+		/* 성별(M:남성,W:여성)를 입력받아 여성인지 확인하는 코드를 작성하세요. 조건 연산자 이용
+		(package : day3.homework, file : Ex1_conditional) 
+		input gender : 
+		M
+		Are you a women? false
+
+		input gender : 
+		W
+		Are you a women? true
+		*/
+
+		Scanner sc = new Scanner(system.in);
+		
+		System.out.println("성별을 입력하시오");
+		
+		
+		int sc = sc.nextInt();
+		
+		String str1;
+		str1 = sc == m ? "true" : "false";
+		
+				
+		System.out.println("당신은 여성입니까?" + str1 );
+		
+		int sr = sr.nextInt();
+		
+		String str2;
+		str2 = sr == m ? "true" : "false";
+		
+		System.out.println("당신은 여성입니까? " + str2);
+		
+		sc.close();
+		
+		
+		
+
+	}
+
+}
---
+++ java/src/day3/homework/Ex6.java
@@ -0,0 +1,34 @@
+package src.day3.homework;
+
+public class Ex6 {
+
+	public static void main(String[] args) {
+		/*성별(M:남성,W:여성)를 입력받아 M이면 남성을, W이면 여성을 출력하는 코드를 작성하세요. if문 이용
+		(package : day3.homework, file : Ex2_if) 
+		input gender : 
+		M
+		Are you a women? man
+
+		input gender : 
+		W
+		Are you a women? woman
+		*/
+		
+
+		Scanner sc = new Scanner(system.in);
+		
+		System.out.println("성별을 입력하시오");
+		
+		int sc = sc.nextInt();
+		
+		if(sc == m) {
+			System.out.println("남자입니다");
+		}
+		if(sc == w) {
+			System.out.println("여성입니다");
+		}
+		sc.close();
+
+	}
+
+}
