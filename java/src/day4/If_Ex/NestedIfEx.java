0c2709cf502f4cfe8a952d7129e370cc442ee4c8
JungByeongChan <gun6377@naver.com>
2023-06-02 14:36:35 +0900

    자바 중첩 if문 예제

---
+++ java/src/day4/If_Ex/NestedIfEx.java
@@ -0,0 +1,43 @@
+package day4.If_Ex;
+
+public class NestedIfEx {
+
+	public static void main(String[] args) {
+		/*
+		 * 2,3,6의 배수 판별 예제
+		 * (Day4.practice패키지에 IfMultipleEx2 문제)
+		if(num1 % 6 == 0) {
+			System.out.println(num1 + " = " + "" + "multiple of 6");
+		}else if(num1 % 2 == 0) {
+			System.out.println(num1 + " = " + "" + "multiple of 2");
+		}else if(num1 % 3 == 0) {
+			System.out.println(num1 + " = " + "" + "multiple of 3");
+		}
+		else {
+			System.out.println(num1 + " = " + "" + "not multiple of 2,3,6");
+		}
+		*/
+		int num = 5;
+		
+		//2의 배수
+		if(num % 2 == 0) {
+			//2의 배수 안에서 3의 배수를 찾는 것 ==> 6의 배수
+			if(num % 3 == 0) {
+				System.out.println(num + " = " + "" + "multiple of 6");
+			}else {
+				System.out.println(num + " = " + "" + "multiple of 2");
+			}
+			//2의 배수 안에서 3의 배수가 아닌 수 ==> 2의 배수
+		}
+		//3의 배수
+		else if(num % 3 == 0) {
+			System.out.println(num + " = " + "" + "multiple of 3");
+		}
+		//아닌 수
+		else {
+			System.out.println(num + " = " + "" + "not multiple of 2,3,6");
+		}
+
+	}
+
+}
