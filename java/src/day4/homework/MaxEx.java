135fafb6ef361bce9249a4ed37f037e63b3c7414
JungByeongChan <gun6377@naver.com>
2023-06-02 14:45:12 +0900

    자바 4일차 과제

---
+++ java/src/day4/homework/MaxEx.java
@@ -0,0 +1,10 @@
+package day4.homework;
+
+public class MaxEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ java/src/day4/homework/ScoreAverageEx.java
@@ -0,0 +1,10 @@
+package day4.homework;
+
+public class ScoreAverageEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
