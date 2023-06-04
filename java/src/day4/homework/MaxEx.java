4db35da06bb7ca3832d5b807acab4fd9181b3d44
JungByeongChan <gun6377@naver.com>
2023-06-05 01:39:47 +0900

    자바 4일차 과제

--- java/src/day4/homework/MaxEx.java
+++ java/src/day4/homework/MaxEx.java
@@ -1,9 +1,37 @@
 package day4.homework;
 
+import java.util.*;
+
 public class MaxEx {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		 * 국어, 영어, 수학 성적을 입력받아 평균을 출력하는 코드를 작성하세요.
+		 
+		 (package : day4.homework, file : ScoreAverageEx)성적은 정수.
+		 */
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("input your korean score : ");
+		
+		int korean = sc.nextInt();
+		
+		System.out.print("input your english score : ");
+		
+		int english = sc.nextInt();
+		
+		System.out.print("input your math score : ");
+		
+		int math = sc.nextInt();
+		
+		String str =  "" + (korean + english + math) / 3.0 ;
+		
+		System.out.print(str);
+		
+		System.out.print((korean + english + math) / 3);
+
 
 	}
 
--- java/src/day4/homework/NumberEx.java
+++ java/src/day4/homework/NumberEx.java
@@ -1,9 +1,31 @@
 package day4.homework;
 
+import java.util.*;
+
 public class NumberEx {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		두 정수를 입력받아 두 정수 중 큰 수를 출력하는 코드를 작성하세요.
+		(package : day4.homework, file : MaxEx)
+		*/
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("두 정수를 입력하세요");
+		
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		if(num1 > num2) {
+			System.out.println(num1 + " 과" + num2 + " 중에서 더 큰 숫자는 " + num1 + "입니다.");
+		}else {
+			System.out.println(num1 + " 과" + num2 + " 중에서 더 큰 숫자는 " + num2 + "입니다.");
+		}
+		
+		
+		sc.close();
+		
+
 
 	}
 
