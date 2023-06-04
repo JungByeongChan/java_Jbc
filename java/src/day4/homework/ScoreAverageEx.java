9511fe6d257dd5ed2757cad83fe95ea9ce134721
JungByeongChan <gun6377@naver.com>
2023-06-05 01:38:36 +0900

    자바 4일차 과제

--- java/src/day4/homework/ScoreAverageEx.java
+++ java/src/day4/homework/ScoreAverageEx.java
@@ -1,9 +1,42 @@
 package day4.homework;
 
+import java.util.*;
+
 public class ScoreAverageEx {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		세 변의 길이를 정수로 입력받아 세 변으로 삼각형을 만들 수 있는지 확인하는 코드를 작성하세요.
+		(package : day4.homework, file : TriangleEx)
+		세 변 중 작은 두 변의 길이의 합이 큰 변보다 커야 삼각형을 만들 수 있다.
+		input 3 integer num : 
+		1 2 3 
+		Unable to create triangle.
+		input 3 integer num : 
+		2 2 3 
+		Able to create triangle.
+		*/
+		
+		Scanner sr = new Scanner(System.in);
+		System.out.println("정수를 입력하세요.");
+		
+		int num1 = sr.nextInt();
+		
+		System.out.println("정수를 입력하세요.");
+		int num2 = sr.nextInt();
+		
+		System.out.println("정수를 입력하세요.");
+		int num3 = sr.nextInt();
+		
+		if((num1 + num2) >= num3) {
+			System.out.println(num1 + "+" + num2 + "=" + num3 + " 따라서" + " 삼각형이 만들어질 수 있습니다.");
+		}else {
+			System.out.println(num1 + "+" + num2 + "=" + num3 + " 따라서" + " 삼각형이 만들어질 수 없습니다.");
+		}
+		
+		String str = (num1 + num2) >= num3 ? "가능합니다" : "불가능합니다" ;
+		
+		System.out.println(num1 + "+" + num2 + "=" + num3 + " 따라서" + str);
 
 	}
 
--- java/src/day4/homework/TriangleEx.java
+++ java/src/day4/homework/TriangleEx.java
@@ -1,9 +1,35 @@
 package day4.homework;
 
+import java.util.Scanner;
+
 public class TriangleEx {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		정수 num가 짝수이면 num에 2를 나누고, 정수 num가 홀수이면 num에 1을 더한 후, num를 출력하는 코드를 작성하세요.
+		(package : day4.homework, file : NumberEx)
+		*/
+		
+		System.out.println("inpur number : ");
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		
+		
+		if(num % 2 == 0) {
+			System.out.println(num / 2);
+		}else {
+			System.out.println(num + 1);
+		}
+		
+		
+		
+		
+		
+		sc.close();
 
 	}
 
