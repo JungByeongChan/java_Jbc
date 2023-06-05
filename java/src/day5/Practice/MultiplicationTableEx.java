48852e79dc7b38bbe292ba49585b9162457bd1ee
JungByeongChan <gun6377@naver.com>
2023-06-05 15:03:03 +0900

    자바 구구단 예제

---
+++ java/src/day5/Practice/MultiplicationTableEx.java
@@ -0,0 +1,26 @@
+package day5.Practice;
+
+public class MultiplicationTableEx {
+
+	public static void main(String[] args) {
+		// 구구단 2단을 출력하는 코드를 작성하시오
+		
+		int i = 1;
+		
+		
+		/*
+		int E = i * max++;
+		
+		System.out.println(E);
+		*/
+				
+		
+		while(i <= 9) {
+			System.out.println("2 X " + i + "=" + 2*i);
+			++i;
+			
+		}
+		
+	}
+
+}
