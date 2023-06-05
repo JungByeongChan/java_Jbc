125ee827f007178773178ea9e6cb0fe3a2baf015
JungByeongChan <gun6377@naver.com>
2023-06-05 15:03:19 +0900

    자바 5일차 과제

---
+++ java/src/day5/homework/TeamLeaderEx.java
@@ -0,0 +1,23 @@
+package day5.homework;
+
+public class TeamLeaderEx {
+
+	public static void main(String[] args) {
+		// 30명의 학생이 있는 반에서 1번부터 연속된 번호를 가진 3명의 학생이 1조가 되도록 나누었다
+		// 이 때 가장 큰 번호인 학생이 조장이된다
+		// 학생 번호가 주어졌을 때 조장인지 조원인지 판별하는 코드를 작성하세요.
+		/*
+		(switch문으로)
+		input student number :
+		1
+		student number 1 is a member of the team.
+		input student number :
+		6
+		student number 6 is the team leader.
+		*/
+		
+			
+
+	}
+
+}
---
+++ java/src/day5/homework/UpDownEx.java
@@ -0,0 +1,24 @@
+package day5.homework;
+
+public class UpDownEx {
+
+	public static void main(String[] args) {
+		/*
+		  updown 게임
+		  1~100 사이의 랜덤한 정수를 생성하고, 이 정수를 맞추는 게임을 구현하세요.
+		  
+		  랜덤한 수 : 45
+		  input : 50
+		  down!
+		  input : 40
+		  up!
+		  input : 45
+		  Good!
+		  
+		  맞출때까지 구현, good이 나올때까지 구현하면됨
+		  
+		 */
+
+	}
+
+}
