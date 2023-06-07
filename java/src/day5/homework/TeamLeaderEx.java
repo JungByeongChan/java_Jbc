2a416c4f6ba835457d359ba318e6ad684cbfc441
JungByeongChan <gun6377@naver.com>
2023-06-07 09:36:32 +0900

    자바 5일차 과제 문제풀이

--- java/src/day5/homework/TeamLeaderEx.java
+++ java/src/day5/homework/TeamLeaderEx.java
@@ -1,5 +1,7 @@
 package day5.homework;
 
+import java.util.Scanner;
+
 public class TeamLeaderEx {
 
 	public static void main(String[] args) {
@@ -15,15 +17,16 @@ public class TeamLeaderEx {
 		6
 		student number 6 is the team leader.
 		*/
+		
+		
 		int min = 1, max = 30;
 		int random = (int)(Math.random() * (max - min + 1) + min);
 		
 		System.out.println(random);
+			
 		
-		
-		
-		switch(random) {
-			case 3,6,9,12,15,18,21,24,27,30 :
+		switch(random % 3) {
+			case 0 :
 				System.out.println(random + " 번의 학생은 " + "Team leader" + " 이다");
 			break;
 			default :
@@ -39,8 +42,28 @@ public class TeamLeaderEx {
 		
 		System.out.println(random + " 번의 학생은 " + leader1 + " 이다"); 
 		
-			
-
+		// 이하의 코딩은 강사풀이
+		int num;
+		Scanner sc = new Scanner(System.in);
+				
+		//학생 번호를 입력
+		System.out.println("input student number : ");
+		num = sc.nextInt();
+				
+		
+		//학생 번호가 팀장번호인지 아닌지를 판별
+		//번호가 3 의 배수이면 팀장이라고 출려갛고 아니면 팀원이라고 출력
+		//num을 3으로 나누었을 때 나머지가 0과 같다면 팀장, 아니면 팀원으로 출력
+		//num % 3 == 0
+		switch(num % 3) {
+		case 0 :
+			System.out.println("student number " + num + " is the team leader");
+			break;
+			default:
+			System.out.println("student number " + num + " is a meber of the team.");
+		}
+		sc.close();
+		
 	}
 
 }
--- java/src/day5/homework/UpDownEx.java
+++ java/src/day5/homework/UpDownEx.java
@@ -39,6 +39,7 @@ public class UpDownEx {
 			
 		if(num == random) {
 				System.out.println("Good!");
+				break;
 				
 			}else if(num < random) {
 				System.out.println("up!");
@@ -48,7 +49,27 @@ public class UpDownEx {
 				System.out.println("Down!");
 			}
 		 System.out.println("input number : ");
+		 
 		 num = sc.nextInt();
+		}
+		 // 이하는 강사풀이
+		 // 반복문 : 맞출때까지 => 입력한 정수가 정답이 아니면 반복
+		 int num1 = min - 1; // 1~100이 아닌 정수로 초기화 하면 됨. 최소값 -1을 해서 랜덤으로 나올 수 없는 수
+		 
+		 while(random != num1) {
+			 //정수를 입력
+			 System.out.println("input number(1~100) : ");
+			 num1 = sc.nextInt();
+			 //정수와 정답을 비교하여 판별
+			 //num1이 random보다 크면 Down!, num1이 random보다 작으면 Up!, 아니면 Good!
+			if(num1 > random){
+				System.out.println("Down!");
+			}else if(num < random) {
+				System.out.println("Up!");
+			}else {
+				System.out.println("Good!");
+			}
+		 }
 		 
 		
 			
@@ -56,7 +77,7 @@ public class UpDownEx {
 			
 	}
 		
-}
+
 		
 		
 		
