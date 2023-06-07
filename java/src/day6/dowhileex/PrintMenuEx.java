be84c7be56f14321b318872858750d9980b4dd2f
JungByeongChan <gun6377@naver.com>
2023-06-07 14:47:57 +0900

    자바 6일차 메뉴반복 예제

---
+++ java/src/day6/dowhileex/PrintMenuEx.java
@@ -0,0 +1,46 @@
+package day6.dowhileex;
+
+import java.util.Scanner;
+
+public class PrintMenuEx {
+
+	public static void main(String[] args) {
+		// 다음과 같은 메뉴를 출력하고 프로그램 종료를 선택하기 전까지 반복적으로 실행되는 코드를 작성하시오
+		/* menu
+		 * 1. Start
+		 * 2. Save
+		 * 3. Exit
+		 * Select Menu :
+		 * 
+		 */
+		int menu;
+		Scanner sc = new Scanner(System.in);
+		//입력받은 menu가 3이 아니면 반복
+		do {
+			//메뉴 출력 및 입력
+			System.out.println("menu");
+			System.out.println("1. start");
+			System.out.println("2. save");
+			System.out.println("3. exit");
+			System.out.println("Select Menu : ");
+			menu = sc.nextInt();
+			//선택한 메뉴에 따른 출력문 실행
+			switch(menu) {
+			case 1:
+				System.out.println("Program Start!");
+				break;
+			
+			case 2:
+				System.out.println("Program Save!");
+				break;
+			case 3:
+				System.out.println("Program Exit!");
+				break;
+			
+			}
+		}while(menu != 3);
+		sc.close();
+
+	}
+
+}
