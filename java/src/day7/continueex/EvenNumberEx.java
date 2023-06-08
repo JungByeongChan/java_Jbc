050ca8c86319a056f5e6f595b0009211c736db7c
JungByeongChan <gun6377@naver.com>
2023-06-08 14:43:15 +0900

    자바 7일차 continue 예제 짝수출력

---
+++ java/src/day7/continueex/EvenNumberEx.java
@@ -0,0 +1,34 @@
+package day7.continueex;
+
+public class EvenNumberEx {
+
+	public static void main(String[] args) {
+		// continue 문을 이용한 10 이하 짝수 출력 예제
+		
+		int i;
+		
+		for(i = 1 ; i <= 10 ; i++ ) {
+			// i가 홀수이면 건너 뜀
+			if(i % 2 != 0) {
+				continue;
+			}
+			
+			System.out.println(i);
+		}
+		
+		
+		
+		i = 0;
+		while(++i <= 10) {
+			if(i % 2 != 0) {
+				
+				continue;
+			}
+			System.out.println(i);
+			
+		}
+		
+
+	}
+
+}
