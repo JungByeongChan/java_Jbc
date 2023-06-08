754743512d798a406dc61c236fe4507b2101a07c
JungByeongChan <gun6377@naver.com>
2023-06-08 14:45:33 +0900

    자바 7일차 과제

---
+++ java/src/day7/homework/StarEx5.java
@@ -0,0 +1,19 @@
+package day7.homework;
+
+public class StarEx5 {
+
+	public static void main(String[] args) {
+		/* 아래와 같이 코드를 출력하시오.
+		 	*
+		   ***
+		  *****
+		 *******
+		  *****
+		   ***
+		    *
+		  
+		 */
+
+	}
+
+}
