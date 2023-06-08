34f5a73c10384db9407839e6bf8031dee7518b27
JungByeongChan <gun6377@naver.com>
2023-06-08 14:42:57 +0900

    자바 7일차 break 예제

---
+++ java/src/day7/breakex/HelloWorldEx.java
@@ -0,0 +1,18 @@
+package day7.breakex;
+
+public class HelloWorldEx {
+
+	public static void main(String[] args) {
+		// 
+		int i = 1;
+		while(true) {
+			System.out.println("Hello World");
+			if(i == 5) {
+				break;
+			}
+			i++;
+		}
+
+	}
+
+}
