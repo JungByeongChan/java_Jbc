ed6f45ffffa55a654f8fc82f9b81c0be7d1b8d41
JungByeongChan <gun6377@naver.com>
2023-06-09 09:00:05 +0900

    자바 7일차 과제

--- java/src/day7/homework/StarEx5.java
+++ java/src/day7/homework/StarEx5.java
@@ -14,6 +14,72 @@ public class StarEx5 {
 		  
 		 */
 
+	
+	
+	/*
+	int i, j ;
+	for(i = 1 ; i <= 7 ; i++) {
+		for(j = 1 ; j <= 4 - i ; j++) {
+			System.out.print(" ");
+		}
+		for(j = 1 ; j <= i ; j++) {
+			System.out.print("*");
+		}
+		for(j = 1 ; j <= i-1 ; j++) {
+			System.out.print("*");
+		}
+		for(j = 5 ; j <= i ; j++) {
+			System.out.println(" 5");
+		}
+			System.out.println("");
+	}*/
+	
+	for(int i = 1 ; i <= 4 ; i++) {
+		for(int j = 1 ; j <= 4-i ; j++) {
+			System.out.print(" ");
+		}for(int j = 1 ; j <= 2*i -1 ; j++) {
+			System.out.print("*");
+		}
+		
+		System.out.println();
+	}/*
+	for(int i = 1 ; i <= 4 ; i++) {
+		for(int j = 4 ; j <= 4 + i ; j++){
+			System.out.print("*");
+		}
+		for(int j = 1 ; j <= 2 * i -1 ; j++) {
+			System.out.print(" ");
+		}
+		System.out.println();
+	}*/
+	
+	
+	
+	
+	
+	for(int i = 4 ; i >= 1 ; i--) {
+		for(int j = 1 ; j <= 4 - i ; j++) {
+			System.out.print(" ");
+		}
+		for(int j = 1 ; j <= 2 * i - 1 ; j++) {
+			System.out.print("*");
+		}//for(int j = 1 ; j <= 1; j++) {
+		//	System.out.print("*");
+		//}
+		System.out.println();
 	}
-
+	
+	
+	/*
+	for(int i = 1 ; i <= 3 ; i++) {
+		for(int j = 1 ; j <= i ; j++) {
+			System.out.print("*");
+		}for(int j = 1 ; j <= 2 * i - 1 ; j++) {
+			System.out.print(" ");
+		}
+		System.out.println("");
+		
+	}*/
+	}	
 }
+
