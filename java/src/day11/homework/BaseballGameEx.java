46da72bf529b05559808386a9981d392d3817dd6
JungByeongChan <gun6377@naver.com>
2023-06-14 15:01:43 +0900

    자바 11일차 과제

---
+++ java/src/day11/homework/BaseballGameEx.java
@@ -0,0 +1,20 @@
+package day11.homework;
+
+public class BaseballGameEx {
+
+	public static void main(String[] args) {
+		/*
+		  	숫자 야구 게임을 구현하시오.
+		  	1~9 사이의 중복되지 않은 세 수를 맞추는 게임
+		  	규칙 :
+		  	B : 같은 숫자가 있지만 위치가 다름
+		  	S : 같은 숫자가 있고 위치가 같음
+		  	3O : 일치하는 숫자가 하나도 없음
+		  	
+		  	
+		  	
+		 */
+
+	}
+
+}
