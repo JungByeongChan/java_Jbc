ba6c10ee1f87d00da42828989de78c2c149c612f
JungByeongChan <gun6377@naver.com>
2023-06-15 14:49:34 +0900

    자바 12일차 과제

---
+++ java/src/day12/homework/LottoEx.java
@@ -0,0 +1,21 @@
+package day12.homework;
+
+public class LottoEx {
+
+	public static void main(String[] args) {
+		// 로또 번호를 생성하고, 사용자가 번호를 입력하면 몇등인지 출력하는 코드를 작성하세요.
+		/*
+		  1등 : 번호 6개
+		  2등 : 번호 5개 + 보너스
+		  3등 : 번호 5개
+		  4등 : 번호 4개
+		  5등 : 번호 3개
+		  number : [1,3,40,23,24,45]
+		  bonus : 2
+		  user : 1,2,3,4,5,6
+		  losing ticket!(꽝)
+		 */
+
+	}
+
+}
