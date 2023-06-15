8fdd996d83d1a65508c0de04fabc6728122f226e
JungByeongChan <gun6377@naver.com>
2023-06-15 14:49:21 +0900

    자바 단어장 클래스 추가 및 테스트

---
+++ java/src/day12/practice/VocabularyNoteEx1.java
@@ -0,0 +1,14 @@
+package day12.practice;
+
+public class VocabularyNoteEx1 {
+	public static void main(String[] args) {
+		
+		Word wordList[] = {
+				new Word("abc"),
+				new Word("def", "def is define")
+		};
+		VocabularyNote note = new VocabularyNote(wordList);
+		note.print();
+		
+	}
+}
