f197c15d6640f5296ddd697a78b0f275fd9b196a
JungByeongChan <gun6377@naver.com>
2023-06-16 12:36:39 +0900

    자바 로또 과제 풀이

--- java/src/day12/homework/LottoEx.java
+++ java/src/day12/homework/LottoEx.java
@@ -19,8 +19,51 @@ public class LottoEx {
 		  user : 1,2,3,4,5,6
 		  losing ticket!(꽝)
 		 */
-
 		
+		//랜덤으로 로또번호 6개와 보너스 번호 1개를 생성
+		//7개짜리 배열에 로또 번호를 생성하고, 앞에 6개는 로또번호, 마지막 1개는 보너스로 활용
+		int lotto[] = new int[7];
+		//7개짜리 로또번호 생성
+		Array.createRandomArray(1, 45, lotto);
+		
+		//마지막 1개는 보너스로
+		int bonus = lotto[6];
+		int lotto2[] = new int[6];
+		//로또1의 0번지부터 로또2의 0번지부터 6번지까지 붙여넣기
+		System.arraycopy(lotto, 0, lotto2, 0, 6);
+		//로또 번호 정렬
+		Array.sort(lotto2);
+		
+		System.out.println("number : ");
+		Array.printArray(lotto2);
+		System.out.println("bonus : " + bonus);
+		
+		//사용자가 번호를 6개 입력
+		Scanner sc = new Scanner(System.in);
+		int user[] = new int[6];
+		
+		System.out.println("user : ");
+		for(int i = 0 ; i < user.length ; i++) {
+			user[i] = sc.nextInt();
+		}
+				
+		//중복입력하면 판별을 X
+		if(Array.arrayCheck(user)) {
+			System.out.println("Wrong number!");
+			return;
+		}
+		
+		
+		//등수 판별
+		int count = count(lotto2, user);
+		printResult(count, Array.contains(user, bonus, user.length));
+		
+		
+		sc.close();
+		
+		
+
+		/*
 		Scanner sc = new Scanner(System.in);
 		
 		
@@ -64,18 +107,60 @@ public class LottoEx {
 		
 		
 		sc.close();
+	
+		
+		*/
 	}
-		
-		
-		
-		
-		
-		
-		
-}
-
-
-class atari{
+	/**
+	 * 로또 번호와 사용자 번호가 주어지면 몇개가 같은지 알려주는 메서드
+	 * 매개변수 : 로또 번호와 사용자 번호 => int lotto[], int user[]
+	 * 리턴타입 : 몇개가 같은지 => 정수 = > int
+	 * 메서드명 : count
+	 *  */
 	
-	
+	public static int count(int lotto[], int user[]) {
+		if(lotto == null || user == null) {
+			return 0;
+		}
+		
+		int count = 0;
+		for(int tmp : lotto) {
+			if(Array.contains(user, tmp, user.length)) {
+				count++;
+			}
+		}
+		return count;
+	}
+	/**
+	 * 일치하는 개수와 보너스번호와 사용자 번호가 일치하는 여부를 알려주면 등수를 출력하는 메서드
+	 * 매개변수 : 일치하는 개수, 보너스 번호와 일치 여부
+	 * 			=> int count, boolean checkBonus
+	 * 리턴타입 : 없음 => void
+	 * 메서드명 : printResult
+	 *  */
+	public static void printResult(int count, boolean checkBonus) {
+		switch(count) {
+		case 6:
+			System.out.println("1st");
+			break;
+		case 5:
+			if(checkBonus) {
+				System.out.println("2st");
+			}
+			else {
+			System.out.println("3st");
+			}
+			break;
+		case 4:
+			System.out.println("4st");
+			break;
+		case 3:
+			System.out.println("5st");
+			break;
+		default:
+			System.out.println("Losing ticket!");
+				
+		}
+	}
+
 }
