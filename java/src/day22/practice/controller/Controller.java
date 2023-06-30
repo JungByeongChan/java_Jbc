2166a0689ef924a6982a564fed9ba26c8f0979b8
JungByeongChan <gun6377@naver.com>
2023-06-30 15:06:47 +0900

    전화번호 관리 프로그램 예제(도중)

--- java/src/day22/practice/controller/Controller.java
+++ java/src/day22/practice/controller/Controller.java
@@ -2,7 +2,9 @@ package day22.practice.controller;
 
 import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
+import java.util.function.Predicate;
 
 import day22.practice.vo.PhoneList;
 
@@ -12,7 +14,9 @@ public class Controller implements Program2{
 	private final int Exit = 5;
 	
 	private ArrayList<PhoneList> list = new ArrayList<>();
-
+	private List<PhoneList> list2 = new ArrayList<>();
+	
+	
 	@Override
 	public void printMenu() {
 		System.out.println("===========");
@@ -80,7 +84,7 @@ public class Controller implements Program2{
 		
 		System.out.println("삭제할 번호 입력 :");
 		sc.nextLine();
-		int num = sc.nextInt();
+		String num = sc.nextLine();
 		
 		PhoneList phone = new PhoneList(name, num);
 		int index = list.indexOf(phone);
@@ -106,7 +110,7 @@ public class Controller implements Program2{
 		System.out.println("변경할 번호의 이름을 입력 : ");
 		String name = sc.nextLine();
 		
-		PhoneList phone = new PhoneList(name, 0);
+		PhoneList phone = new PhoneList(name, null);
 		int index = list.indexOf(phone);
 		
 		if(index == -1) {
@@ -115,7 +119,7 @@ public class Controller implements Program2{
 		}
 		sc.nextLine();
 		System.out.println("바꿀 번호를 입력");
-		int num = sc.nextInt();
+		String num = sc.nextLine();
 		phone = new PhoneList(name, num);
 		if(list.contains(phone)) {
 			System.out.println("등록 실패");
@@ -128,13 +132,108 @@ public class Controller implements Program2{
 		
 		
 	}
+	public boolean insertPhone(String name, String phoneNumber) {
+		PhoneList pn = new PhoneList(name, phoneNumber);
+		if(pn.getPhoneNumber().length() == 0) {
+			return false;
+		}
+		list2.add(pn);
+		return true;
+	}
+	
+	public void print(Predicate<PhoneList> p) {
+		for(PhoneList pn : list2) {
+			if(p.test(pn)) {
+				System.out.println(pn);
+			}
+		}
+	}
+	
+	public boolean updateName(String name, String updateName) {
+		//name과 일치하는 전화번호를 찾아야 함
+		//전화번호부에 수정할 이름이 해당하는 전화번호가 어디에 있는지 확인
+		int index = list2.indexOf(new PhoneList(updateName,""));
+		//없으면 수정이 안됨 => 메서드를 종료
+		if(index == -1) {
+			return false;
+		}
+		//찾은 전화번호의 이름을 수정
+		//전화번호부에서 이름과 일치하는 전화번호를 가져옴
+		PhoneList pn = list2.get(index);
+		//가져온 전화번호와 이름을 수정
+		pn.setName(updateName);
+		
+		//list2.get(index).setName(updateName); <= 위의 코드2줄을 한줄로 정리하면 이렇게가능
+		
+		/*
+		// 기본 자료형은 =을 이용하면 값을 복사하기 때문에 한쪽의 값이 바뀌어도 다른쪽은 바뀌지않는다.
+		int num1 = 1; // num1 = 1
+		int num2 = num1; // num1 = 1, num2 = 1
+		num2 = 10;		// num1 = 1, num2 = 10
+		//참조변수는 =을 이용하면 주소값을 복사하기 때문에 한쪽의 값이 바뀌면 다른쪽도 바뀜
+		//Ex) pn1에 저장된 주소가 0x1000
+		PhoneList pn1 = new PhoneList("a", "010-1234-5678");
+		//pn1에 저장된 주소 : 0x1000, pn2에 저장된 주소 : 0x1000
+		PhoneList pn2 = pn1;
+		//0x1000번지에 있는 객체의 name을 Lim으로 수정
+		pn2.setName("Lim");
+		*/
+		return true;
+	}
+	//전화번호 수정
+	public boolean updateNumber(String name, String phoneNumber) {
+		//name과 일치하는 전화번호부를 찾음
+		int index = list2.indexOf(new PhoneList(name, ""));
+		//일치하는 전화번호부가 없으면 수정 실패
+		if(index == -1) {
+			return false;
+		}
+		//number가 전화번호 패턴이 맞는지 확인하기 위해서
+		//전화번호 패턴이 맞으면 전홥너호 객체를 생성 했을 때 전화번호가 있고,
+		//패턴이 맞지 않으면 빈 문자열이 있다.
+		//name과 number를 이용하여 전화번호 객체를 생성하는 대신 checkNumber를 이용
+		//number가 전화번호 패턴이면 전화번호룰 수정
+		if(!PhoneList.checkNumber(phoneNumber)) {
+			return false;
+		}
+		list2.get(index).setNumber(phoneNumber);
+		return true;
+	}
+	
+	public boolean update(String name, String updateName, String phoneNumber) {
+		PhoneList pn = new PhoneList(name, phoneNumber);
+		//name을 이용하여 전화번호부 검색
+		int index = list2.indexOf(pn);
+		//없으면 수정 실패
+		if(index == -1) {
+			return false;
+		}
+		//number가 전화번호 패턴에 안맞으면 실패
+		if(!PhoneList.checkNumber(phoneNumber)) {
+			return false;
+		}
+		//기존에 있는 전화번호를 삭제
+		list.remove(index);
+		//updateName과 number를 이용하여 전화번호 객체를 생성한 후 추가
+		list.add(pn);
+		return true;
+	}
+	
+	public boolean delete(String name) {
+		//name으로 PhoneNumber 객체를 생성 후 삭제 요청
+		return list.remove(new PhoneList(name, ""));
+	}
+	
+	
+	
+	
 
 	private void phoneNumberRegistration() {
 		System.out.println("이름 입력 :");
 		String name = sc.nextLine();
 		System.out.println("번호 입력");
 		sc.nextLine();
-		int num = sc.nextInt();
+		String num = sc.nextLine();
 		
 		PhoneList phone = new PhoneList(name, num);
 		if(!list.contains(phone)) {
--- java/src/day22/practice/vo/PhoneList.java
+++ java/src/day22/practice/vo/PhoneList.java
@@ -1,20 +1,42 @@
 package day22.practice.vo;
 
 import java.util.Objects;
+import java.util.regex.Pattern;
 
+import lombok.Data;
+
+@Data
 public class PhoneList {
 
-	int phoneNumber;
-	String name;
+	private String phoneNumber;
+	private String name;
 	
-	public PhoneList(String name, int phoneNumber) {
+	//전화번호가 010-xxxx-xxxx 패턴이 맞는지 확인해주는 메서드
+	//외부에서도 객체생성 없이 쓸 수 있게 static과 public을 붙여줌 // static이 붙으면 객체생성 없이 이용가능
+	public static boolean checkNumber(String num) {
+		String regex = "^010(-\\d{4}){2}&";
+		return Pattern.matches(regex, num);
+	}
+	
+	
+	public PhoneList(String name, String phoneNumber) {
+		if(checkNumber(phoneNumber)) {
 		this.phoneNumber = phoneNumber;
+		};
+		this.phoneNumber = checkNumber(phoneNumber)?phoneNumber:"";
 		this.name = name;
 	}
+	
+	
+	public void setNumber(String phoneNumber) {
+		this.phoneNumber = checkNumber(phoneNumber)?phoneNumber:this.phoneNumber;
+	}
 
-	public void update(String name, int num) {
+	public void update(String name, String phoneNumber) {
 		// TODO Auto-generated method stub
-		this.phoneNumber = num;
+		if(checkNumber(phoneNumber)) {
+			this.phoneNumber = phoneNumber;
+			};
 		this.name = name;
 	}
 
